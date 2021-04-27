c5d7ce7f710999d83dc0e4951b10fe0c59c8204d
Podesta, Sergio <spodesta@omnitracs.com>
2021-04-27 16:30:39 -0500

    Section 2

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category;
+            return View(objList);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category newCategory)
+        {
+            _db.Category.Add(newCategory);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
