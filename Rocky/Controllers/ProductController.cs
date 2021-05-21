f0d055cf0d5fd709bd8361d62c8c2005daf22951
Podesta, Sergio <spodesta@omnitracs.com>
2021-05-21 16:41:10 -0500

    Section 4

---
+++ Rocky/Controllers/ProductController.cs
@@ -0,0 +1,199 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Rocky.Data;
+using Rocky.Models;
+using Rocky.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ApplicationDbContext _db;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+
+        public ProductController(ApplicationDbContext db, IWebHostEnvironment webHostEnvironment)
+        {
+            _db = db;
+            _webHostEnvironment = webHostEnvironment;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objList = _db.Product;
+
+            foreach (var obj in objList)
+            {
+                obj.Category = _db.Category.FirstOrDefault(u => u.Id == obj.CategoryId);
+            }
+
+            return View(objList);
+        }
+
+        [HttpGet]
+        public IActionResult Upsert(int? id)
+        {
+            //IEnumerable<SelectListItem> CategoryDropDown = _db.Category.Select(i => new SelectListItem
+            //{
+            //    Text = i.Name,
+            //    Value = i.Id.ToString()
+            //});
+
+            //ViewBag.CategoryDropDown = CategoryDropDown;
+
+            //Product product = new Product();
+
+            ProductVM productVM = new ProductVM()
+            {
+                Product = new Product(),
+                CategorySelectList = _db.Category.Select(i => new SelectListItem
+                {
+                    Text = i.Name,
+                    Value = i.Id.ToString()
+                })
+            };
+
+            if (id == null)
+            {
+                //Create
+                return View(productVM);
+            }
+            else
+            {
+                //Edit
+                productVM.Product = _db.Product.Find(id);
+                if (productVM.Product == null)
+                {
+                    return NotFound();
+                }
+                
+                return View(productVM);
+            }
+            
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(ProductVM newProduct)
+        {
+            if (ModelState.IsValid)
+            {
+
+                var files = HttpContext.Request.Form.Files;
+                string webRootPath = _webHostEnvironment.WebRootPath;
+
+                if (newProduct.Product.Id == 0)
+                {
+                    //create
+                    string upload = webRootPath + WC.ImagePath;
+                    string fileName = Guid.NewGuid().ToString();
+                    string extension = Path.GetExtension(files[0].FileName);
+                    string fileFullName = Path.Combine(upload, fileName + extension);
+
+                    using (var fileStream = new FileStream(fileFullName, FileMode.Create))
+                    {
+                        files[0].CopyTo(fileStream);
+                    }
+
+                    newProduct.Product.Image = fileName + extension;
+
+                    _db.Product.Add(newProduct.Product);
+                    _db.SaveChanges();
+
+                }
+                else
+                {
+                    //update
+                    var objFromDb = _db.Product.AsNoTracking().FirstOrDefault(u => u.Id == newProduct.Product.Id);
+
+                    if (files.Count > 0)
+                    {
+                        string upload = webRootPath + WC.ImagePath;
+                        string fileName = Guid.NewGuid().ToString();
+                        string extension = Path.GetExtension(files[0].FileName);
+                        string fileFullName = Path.Combine(upload, fileName + extension);
+
+                        var oldFile = Path.Combine(upload, objFromDb.Image);
+
+                        if (System.IO.File.Exists(oldFile))
+                        {
+                            System.IO.File.Delete(oldFile);
+                        }
+
+                        using (var fileStream = new FileStream(fileFullName, FileMode.Create))
+                        {
+                            files[0].CopyTo(fileStream);
+                        }
+
+                        newProduct.Product.Image = fileFullName;
+                    }
+                    else
+                    {
+                        newProduct.Product.Image = objFromDb.Image;
+                    }
+                    _db.Product.Update(newProduct.Product);
+                    _db.SaveChanges();
+                }
+
+                return RedirectToAction("Index");
+            }
+            newProduct.CategorySelectList = _db.Category.Select(i => new SelectListItem
+            {
+                Text = i.Name,
+                Value = i.Id.ToString()
+            });
+            return View(newProduct);
+            
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+            var obj = _db.Product.Include(u => u.Category).FirstOrDefault(u=> u.Id == id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Product.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            {
+                string webRootPath = _webHostEnvironment.WebRootPath;
+                string upload = webRootPath + WC.ImagePath;
+                string fileName = obj.Image;
+                string fileFullName = Path.Combine(upload, fileName);
+
+                if (System.IO.File.Exists(fileFullName))
+                    System.IO.File.Delete(fileFullName);
+
+                _db.Product.Remove(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+        }
+
+    }
+}
